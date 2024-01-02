b9d2d807f60519442a2df07fe26f88ee303680af
vedasole <vedasole001@gmail.com>
2024-01-02 10:33:30 +0530

    initial-commit

---
+++ src/main/java/com/vedasole/ekartecommercebackend/EkartEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vedasole.ekartecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartEcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkartEcommerceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vedasole/ekartecommercebackend/EkartEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedasole.ekartecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkartEcommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
