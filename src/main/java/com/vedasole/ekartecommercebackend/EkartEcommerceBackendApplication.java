b0c7c099b06591ebf5093f24618989db14589c9e
vedasole <vedasole001@gmail.com>
2024-02-26 01:57:35 +0530

    added modelMapper and removed spring data rest

--- src/main/java/com/vedasole/ekartecommercebackend/EkartEcommerceBackendApplication.java
+++ src/main/java/com/vedasole/ekartecommercebackend/EkartEcommerceBackendApplication.java
@@ -1,8 +1,13 @@
 package com.vedasole.ekartecommercebackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * Main class of the E-Kart E-Commerce Backend application.
+ */
 @SpringBootApplication
 public class EkartEcommerceBackendApplication {
 
@@ -10,4 +15,13 @@ public class EkartEcommerceBackendApplication {
         SpringApplication.run(EkartEcommerceBackendApplication.class, args);
     }
 
+    /**
+     * Creates a new instance of the ModelMapper class.
+     *
+     * @return a new ModelMapper instance
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
