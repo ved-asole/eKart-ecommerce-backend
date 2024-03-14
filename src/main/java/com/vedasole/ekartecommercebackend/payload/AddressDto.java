648914d1f100051c0d18d6d34a49861b113b1996
vedasole <vedasole001@gmail.com>
2024-03-14 23:51:03 +0530

    updated dto classes to be immutable through @Value and added validations

--- src/main/java/com/vedasole/ekartecommercebackend/payload/AddressDto.java
+++ src/main/java/com/vedasole/ekartecommercebackend/payload/AddressDto.java
@@ -1,19 +1,52 @@
 package com.vedasole.ekartecommercebackend.payload;
 
 import com.vedasole.ekartecommercebackend.entity.Address;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import org.springframework.hateoas.server.core.Relation;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serial;
 import java.io.Serializable;
 
 /**
  * DTO for {@link Address}
  */
-@Builder
-public record AddressDto(long addressId, String addLine1, String addLine2, @NotNull @NotEmpty @NotBlank String city,
-                         @NotNull @NotEmpty @NotBlank String state, @NotNull @NotEmpty @NotBlank String country,
-                         @Min(5) @Max(value = 6, message = "Postal code should be not greater than 5 characters") int postalCode) implements Serializable {
+@Value
+@AllArgsConstructor
+@Relation(itemRelation = "address", collectionRelation = "addresses")
+public final class AddressDto implements Serializable {
     @Serial
     private static final long serialVersionUID = 4607115799900867635L;
+
+    private final long addressId;
+
+    @NotNull(message = "Address Line 1 is required")
+    @NotBlank(message = "Address Line 1 cannot be blank")
+    private final String addLine1;
+
+    @NotNull(message = "Address Line 2 is required")
+    @NotBlank(message = "Address Line 2 cannot be blank")
+    private final String addLine2;
+
+    @NotNull(message = "City is required")
+    @NotBlank(message = "City cannot be blank")
+    private final String city;
+
+    @NotNull(message = "State is required")
+    @NotBlank(message = "State cannot be blank")
+    private final String state;
+
+    @NotNull(message = "Country is required")
+    @NotBlank(message = "Country cannot be blank")
+    private final String country;
+
+    @NotNull(message = "Postal code is required")
+    @NotBlank(message = "Postal code cannot be blank")
+    @Min(5)
+    @Max(value = 6, message = "Postal code should be not greater than 5 characters")
+    private  final int postalCode;
 }
--- src/main/java/com/vedasole/ekartecommercebackend/payload/CustomerDto.java
+++ src/main/java/com/vedasole/ekartecommercebackend/payload/CustomerDto.java
@@ -1,16 +1,47 @@
 package com.vedasole.ekartecommercebackend.payload;
 
-import javax.validation.constraints.NotNull;
+import com.vedasole.ekartecommercebackend.entity.Customer;
+import lombok.*;
+import org.springframework.hateoas.server.core.Relation;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
- * DTO for {@link com.vedasole.ekartecommercebackend.entity.Customer}
+ * DTO for {@link Customer}
  */
-public record CustomerDto(long customerId, String firstName, String lastName, long phoneNumber, String email,
-                          LocalDateTime createDt, @NotNull AddressDto address) implements Serializable {
+@Value
+@Builder
+@Relation(itemRelation = "customer", collectionRelation = "customers")
+public class CustomerDto implements Serializable {
     @Serial
     private static final long serialVersionUID = -4970632778733952870L;
+
+
+    private final long customerId;
+
+    @NotNull(message = "First name is required")
+    @NotBlank(message = "First name cannot be blank")
+    private final String firstName;
+
+    @NotNull(message = "Last name is required")
+    @NotBlank(message = "Last name cannot be blank")
+    private final String lastName;
+
+    @NotNull(message = "Email is required")
+    @NotBlank(message = "Email cannot be blank")
+    private final String email;
+
+    private final String password;
+
+    @NotNull(message = "Phone number is required")
+    @NotBlank(message = "Phone number cannot be blank")
+    private final long phoneNumber;
+
+    private final LocalDateTime createDt;
+
+    private final AddressDto address;
 }
