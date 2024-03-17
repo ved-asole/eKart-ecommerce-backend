96d8efb8ec6e8ccd2d34274da56a907e5043e7d7
vedasole <vedasole001@gmail.com>
2024-03-17 20:17:19 +0530

    updated api exception to get thrown for other throwable exceptions

--- src/main/java/com/vedasole/ekartecommercebackend/exception/APIException.java
+++ src/main/java/com/vedasole/ekartecommercebackend/exception/APIException.java
@@ -7,4 +7,8 @@ public class APIException extends RuntimeException {
     public APIException(String message) {
         super(message);
     }
+
+    public APIException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
