e5cf73a4293a4bfd8b712fd8419aa56053af09ea
vedasole <vedasole001@gmail.com>
2024-03-18 23:24:23 +0530

    added database validations

--- src/main/java/com/vedasole/ekartecommercebackend/entity/Customer.java
+++ src/main/java/com/vedasole/ekartecommercebackend/entity/Customer.java
@@ -1,12 +1,15 @@
 package com.vedasole.ekartecommercebackend.entity;
 
 
-import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -23,15 +26,39 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
     @SequenceGenerator(name = "customer_seq", allocationSize = 0)
     private long customerId;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "last_name", nullable = false)
     private String lastName;
-    private long phoneNumber;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "email", nullable = false)
     private String email;
-    private String password;
+
+    @Column(name = "create_dt")
     private LocalDateTime createDt;
+
+    @Column(name = "update_dt")
     private LocalDateTime updateDt;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @OneToOne
     @JoinColumn(name = "address_id")
     private Address address;
+
 }
