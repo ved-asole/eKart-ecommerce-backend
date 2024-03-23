7a05081e2fd86d64e7388b54e067bd3e6bfa922c
vedasole <vedasole001@gmail.com>
2024-03-23 23:32:12 +0530

    made ProductDto mutable to allow changes

--- src/main/java/com/vedasole/ekartecommercebackend/payload/ProductDto.java
+++ src/main/java/com/vedasole/ekartecommercebackend/payload/ProductDto.java
@@ -1,7 +1,7 @@
 package com.vedasole.ekartecommercebackend.payload;
 
 import com.vedasole.ekartecommercebackend.entity.Product;
-import lombok.Value;
+import lombok.*;
 import org.springframework.hateoas.server.core.Relation;
 
 import javax.validation.constraints.Min;
@@ -13,36 +13,43 @@ import java.io.Serializable;
 /**
  * DTO for {@link Product}
  */
-@Value
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Relation(itemRelation = "product", collectionRelation = "products")
 public final class ProductDto implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 2079394134035966353L;
 
-    private final long productId;
+    private long productId;
 
     @NotNull(message = "Product name is required")
     @NotBlank(message = "Product name cannot be blank")
-    private final String name;
+    private String name;
 
     @NotNull(message = "Product image is required")
     @NotBlank(message = "Product image cannot be blank")
-    private final String image;
+    private String image;
 
     @NotNull(message = "Product description is required")
     @NotBlank(message = "Product description cannot be blank")
-    private final String desc;
+    private String desc;
 
     @NotNull(message = "Product price is required")
     @Min(value = 0, message = "Product price cannot be negative")
-    private final double price;
+    private double price;
+
+    @NotNull(message = "Product discount is required")
+    @Min(value = 0, message = "Product discount cannot be negative")
+    private double discount;
 
     @NotNull(message = "Product quantity is required")
     @Min(value = 0, message = "Product quantity cannot be negative")
-    private final int qtyInStock;
+    private int qtyInStock;
 
     @NotNull(message = "Category ID is required")
     @Min(value = 1, message = "Category ID cannot be negative")
-    private final long categoryId;
+    private long categoryId;
 }
