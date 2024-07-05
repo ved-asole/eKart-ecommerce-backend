208a713182bf252d17cfa841b2705df3fc3d61f3
vedasole <vedasole001@gmail.com>
2024-07-06 01:03:13 +0530

    moved constants to separate AppConstant file

--- src/main/java/com/vedasole/ekartecommercebackend/utility/AppConstant.java
+++ src/main/java/com/vedasole/ekartecommercebackend/utility/AppConstant.java
@@ -1,27 +1,53 @@
 package com.vedasole.ekartecommercebackend.utility;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 public class AppConstant {
 
+    @AllArgsConstructor
+    @Getter
     public enum RELATIONS {
 
-        CATEGORY("category"),
-        CATEGORIES("categories"),
-        PRODUCT("product"),
-        PRODUCTS("products");
+        CATEGORY("Category"),
+        CATEGORIES("Categories"),
+        PRODUCT("Product"),
+        PRODUCTS("Products"),
+        CUSTOMER("Customer"),
+        CUSTOMERS("Customers"),
+        SHOPPING_CART("ShoppingCart"),
+        SHOPPING_CARTS("ShoppingCarts"),
+        ORDER("Order"),
+        ORDERS("Orders"),
+        USER("User"),
+        USERS("Users");
 
-        private String value;
+        private final String value;
 
-        RELATIONS(String value) {
-            this.value=value;
-        }
+    }
 
-        public String getValue() {
-            return value;
-        }
+    @AllArgsConstructor
+    @Getter
+    public enum OrderStatus {
+        ORDER_CREATED("ORDER_CREATED"),
+        ORDER_PLACED("ORDER_PLACED"),
+        ORDER_FAILED("ORDER_FAILED"),
+        ORDER_DISPATCHED("ORDER_DISPATCHED"),
+        ORDER_SHIPPED("ORDER_SHIPPED"),
+        ORDER_DELIVERED("ORDER_DELIVERED"),
+        ORDER_COMPLETED("ORDER_COMPLETED");
 
-        public void setValue(String value) {
-            this.value = value;
-        }
+        private final String name;
+
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public enum Role {
+        USER("ROLE_USER"),
+        ADMIN("ROLE_ADMIN");
+
+        private final String value;
     }
 
 }
