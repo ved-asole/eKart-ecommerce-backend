1b858d2fafec87e64030d1927ab2f4207cb8e714
vedasole <vedasole001@gmail.com>
2024-08-14 16:25:42 +0530

    tests: added Test cases for User Repo

---
+++ src/test/java/com/vedasole/ekartecommercebackend/repository/UserRepoTest.java
@@ -0,0 +1,66 @@
+package com.vedasole.ekartecommercebackend.repository;
+
+import com.vedasole.ekartecommercebackend.entity.User;
+import com.vedasole.ekartecommercebackend.utility.AppConstant;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+class UserRepoTest {
+
+    @Autowired
+    private UserRepo underTest;
+
+    @BeforeEach
+    void setUp() {
+        //given
+        User user = new User(
+                "john@email.com",
+                "pass@123",
+                AppConstant.Role.USER
+        );
+        underTest.save(user);
+    }
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void shouldFindUserByEmailWhenExists() {
+
+        // Given
+        String email = "john@email.com";
+
+        // When
+        Optional<User> userOptional = underTest.findByEmail(email);
+        boolean foundUser = userOptional.isPresent();
+
+        // Then
+        Assertions.assertTrue(foundUser);
+        assertThat(userOptional.get()).isNotNull().hasFieldOrPropertyWithValue("email", email);
+    }
+
+    @Test
+    void shouldNotFindUserByEmailAndThrowNoSuchElementException() {
+        // Given
+        String email = "random@email.com";
+
+        // When
+        Optional<User> userOptional = underTest.findByEmail(email);
+
+        // Then
+        assertThatThrownBy(userOptional::get).isInstanceOf(NoSuchElementException.class);
+    }
+}
