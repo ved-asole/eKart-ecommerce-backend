b7feb01416e250ae8349e62a86c8f313b81c0e0a
vedasole <vedasole001@gmail.com>
2024-08-17 12:49:14 +0530

    test : Added CategoryController unit test
    - Updated CategoryControllerTest class to CategoryController unit test
    - Moved CategoryControllerITTest integration test to IT folder

--- src/test/java/com/vedasole/ekartecommercebackend/controller/CategoryControllerTest.java
+++ src/test/java/com/vedasole/ekartecommercebackend/controller/CategoryControllerTest.java
@@ -1,57 +1,54 @@
 package com.vedasole.ekartecommercebackend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vedasole.ekartecommercebackend.entity.Category;
 import com.vedasole.ekartecommercebackend.payload.CategoryDto;
-import com.vedasole.ekartecommercebackend.repository.CategoryRepo;
 import com.vedasole.ekartecommercebackend.service.serviceInterface.CategoryService;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CategoryControllerTest {
 
-    private static RestTemplate restTemplate;
     @LocalServerPort
     private int port;
     private String baseUrl="http://localhost";
-    @Autowired
-    private CategoryRepo categoryRepo;
-    @Autowired
+    @MockBean
     private CategoryService categoryService;
+    @Autowired
+    private MockMvc mockMvc;
     private Category expected;
-
-    @BeforeAll
-    static void init() {
-        restTemplate = new RestTemplate();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.getObjectMapper().registerModule(new Jackson2HalModule());
-        restTemplate.getMessageConverters().add(0, converter);
-    }
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
         baseUrl = baseUrl.concat(":").concat(String.valueOf(port)).concat("/api/v1/categories");
 
-        Category category = new Category(
+        expected = new Category(
                 1L,
                 "Mobiles & Tablets",
                 "/images/categories/mobile-and-tablets.webp",
@@ -61,40 +58,27 @@ class CategoryControllerTest {
                 LocalDateTime.now(),
                 LocalDateTime.now()
         );
-        expected = categoryRepo.save(category);
     }
 
-    @AfterEach
-    void tearDown() {
-        categoryRepo.deleteAll();
-    }
-
+    /**
+     * This test method is responsible for creating a new category in the system.
+     *
+     * @throws Exception If any error occurs during the test.
+     */
     @Test
-    void testCreateCategory() {
-        categoryRepo.deleteAll();
-        Category category = new Category(
-                1L,
-                "Mobiles & Tablets",
-                "/images/categories/mobile-and-tablets.webp",
-                "Mobile phones are no more merely a part of our lives. Whether it's to stay connected with friends and family or to keep abreast of important developments around the world, mobiles are no longer for sending a text or making a call. From budget to state-of-the-art smartphones; indigenous names to global big-wigs - a whole universe of mobiles await you on Flipkart. Whether you’re looking for waterdrop notch screens, a high screen to body ratio, AI-powered sensational cameras, high storage capacity, blazing quick processing engines or reflective glass designs, rest assured you won’t have to venture anywhere else for your smartphone needs.",
-                null,
-                true,
-                LocalDateTime.now(),
-                LocalDateTime.now()
-        );
-        ResponseEntity<CategoryDto> categoryDtoResponseEntity = restTemplate.postForEntity(baseUrl, category, CategoryDto.class);
-        assert categoryDtoResponseEntity.getStatusCode().is2xxSuccessful();
-        assertThat(categoryDtoResponseEntity.getBody()).isNotNull();
-        CategoryDto body = categoryDtoResponseEntity.getBody();
-        category.setCategoryId(body.getCategoryId());
-        assertThat(body).isEqualTo(categoryService.convertToDto(category));
+    void testCreateCategory() throws Exception {
+        //given
+        given(categoryService.createCategory(convertToCategoryDto(expected))).willReturn(convertToCategoryDto(expected));
 
-        CategoryDto createCategoryDto = categoryDtoResponseEntity.getBody();
-        assertThat(createCategoryDto.getName()).isEqualTo(category.getName());
-        assertThat(createCategoryDto.getImage()).isEqualTo(category.getImage());
-        assertThat(createCategoryDto.getDesc()).isEqualTo(category.getDesc());
-        assertThat(createCategoryDto.getParentCategory()).isEqualTo(category.getParentCategory());
-        assertThat(createCategoryDto.isActive()).isEqualTo(category.isActive());
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(baseUrl)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(convertToCategoryDto(expected)))
+                )
+               // then
+                .andExpect(status().isCreated());
+        compareCategoryDto("$", resultActions, expected);
     }
 
     /**
@@ -103,56 +87,74 @@ class CategoryControllerTest {
      * @throws Exception If any error occurs during the test.
      */
     @Test
-    void updateCategory() {
+    void updateCategory() throws Exception {
         // Set the updated values for the expected category
         expected.setName("Electronics");
         expected.setImage("/images/categories/electronics.webp");
         expected.setDesc("Choose from a wide range of products such as washing machine, camera, laptop & many more from the top brands & get exciting deals & offers");
         expected.setActive(false);
 
+        given(categoryService.updateCategory(convertToCategoryDto(expected), expected.getCategoryId())).willReturn(convertToCategoryDto(expected));
+
         // Send a PUT request to the server to update the category
-        restTemplate.put(baseUrl.concat("/").concat(String.valueOf(expected.getCategoryId())), expected);
-
-        // Retrieve the updated category from the database
-        Optional<Category> categoryOptional = categoryRepo.findById(expected.getCategoryId());
-        assertTrue(categoryOptional.isPresent());
-        Category updatedCategory = categoryOptional.get();
-
-        // Validate the updated category
-        assertThat(updatedCategory).isNotNull();
-        assertThat(updatedCategory.getCreatedAt()).isBefore(expected.getCreatedAt().plusSeconds(1));
-        assertThat(updatedCategory.getUpdatedAt()).isAfter(expected.getUpdatedAt());
-        expected.setUpdatedAt(updatedCategory.getUpdatedAt());
-        expected.setCreatedAt(updatedCategory.getCreatedAt());
-        assertThat(updatedCategory).isEqualTo(expected);
+        ResultActions resultActions = mockMvc.perform(
+                        put(baseUrl.concat("/").concat(String.valueOf(expected.getCategoryId())))
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(convertToCategoryDto(expected)))
+                )
+                .andExpect(status().isOk());
+        compareCategoryDto("$", resultActions, expected);
     }
 
+    /**
+     * This test method is responsible for retrieving all categories from the system.
+     *
+     * @throws Exception If any error occurs during the test.
+     */
     @Test
-    void deleteCategory() {
-        List<Category> categories = categoryRepo.findAll();
-        if(!categories.isEmpty()) {
-            Category category = categories.get(0);
-            restTemplate.delete(baseUrl.concat("/").concat(String.valueOf(category.getCategoryId())));
-            categoryRepo.findById(category.getCategoryId()).ifPresentOrElse(
-                    c -> Assertions.fail("Category not deleted"),
-                    () -> assertTrue(true)
-            );
-        } else {
-            Assertions.fail("No categories found to delete");
-        }
+    void deleteCategory() throws Exception {
+        //when
+        mockMvc.perform(
+                        MockMvcRequestBuilders.delete(baseUrl.concat("/").concat(String.valueOf(expected.getCategoryId())))
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Category deleted successfully"))
+                .andExpect(jsonPath("$.success").value(true));
     }
 
+    /**
+     * This test method is responsible for retrieving a specific category based on its ID.
+     *
+     * @throws Exception If any error occurs during the test.
+     */
     @Test
-    void getCategory() {
-        ResponseEntity<CategoryDto> categoryDtoResponseEntity = restTemplate.getForEntity(
-                baseUrl.concat("/").concat(Long.toString(expected.getCategoryId())), CategoryDto.class
-        );
-        assert categoryDtoResponseEntity.getStatusCode().is2xxSuccessful();
-        assertThat(categoryDtoResponseEntity.getBody()).isNotNull().isEqualTo(categoryService.convertToDto(expected));
+    void getCategory() throws Exception {
+        //given
+        String categoryId = String.valueOf(expected.getCategoryId());
+        given(categoryService.getCategoryById(expected.getCategoryId())).willReturn(convertToCategoryDto(expected));
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(baseUrl.concat("/").concat(categoryId))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                // then
+                .andExpect(status().isOk());
+        compareCategoryDto("$", resultActions, expected);
     }
 
+    /**
+     * This test method is responsible for retrieving all categories from the system.
+     *
+     * @throws Exception If any error occurs during the test.
+     */
     @Test
-    void getAllCategories() {
+    void getAllCategories() throws Exception {
+        //given
         Category category = new Category(
                 2L,
                 "Electronics",
@@ -163,20 +165,37 @@ class CategoryControllerTest {
                 LocalDateTime.now(),
                 LocalDateTime.now()
         );
-        categoryRepo.save(category);
+        List<Category> categories= new ArrayList<>(List.of(expected, category));
+        List<CategoryDto> categoryDtoList = categories.stream().map(this::convertToCategoryDto).toList();
+        given(categoryService.getAllCategories()).willReturn(categoryDtoList);
 
-        ParameterizedTypeReference<CollectionModel<CategoryDto>> responseType = new ParameterizedTypeReference<>() {};
-        ResponseEntity<CollectionModel<CategoryDto>> responseEntity = restTemplate.exchange(baseUrl, HttpMethod.GET, null, responseType);
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(baseUrl)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.categories").exists())
+                .andExpect(jsonPath("$._embedded.categories").isArray());
+        compareCategoryDto("$._embedded.categories[0]", resultActions, expected);
+        compareCategoryDto("$._embedded.categories[1]", resultActions, category);
+    }
 
-        CollectionModel<CategoryDto> collectionModel = responseEntity.getBody();
-        assertThat(collectionModel).isNotNull();
-        assertThat(collectionModel.getContent()).isNotNull().hasSize(categoryRepo.findAll().size());
-        List<CategoryDto> categoryDtoList = new ArrayList<>(collectionModel.getContent());
-        assertThat(categoryDtoList).isNotNull().contains(categoryService.convertToDto(expected));
-        assertThat(categoryDtoList.stream()
-                        .filter(dto -> dto.getName().equals(category.getName()))
-                        .findFirst()
-        ).isPresent();
+    public void compareCategoryDto(String startingPathExpression, ResultActions resultActions, Category compareWith) throws Exception {
+        if (compareWith== null) compareWith = expected;
+        resultActions.andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath(startingPathExpression + ".categoryId").value(compareWith.getCategoryId()))
+                .andExpect(jsonPath(startingPathExpression + ".name").value(compareWith.getName()))
+                .andExpect(jsonPath(startingPathExpression + ".image").value(compareWith.getImage()))
+                .andExpect(jsonPath(startingPathExpression + ".desc").value(compareWith.getDesc()))
+                .andExpect(jsonPath(startingPathExpression + ".parentCategory").value(compareWith.getParentCategory()))
+                .andExpect(jsonPath(startingPathExpression + ".active").value(compareWith.isActive()));
+    }
+
+    private CategoryDto convertToCategoryDto(Category expected) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(expected, CategoryDto.class);
     }
 
 }
