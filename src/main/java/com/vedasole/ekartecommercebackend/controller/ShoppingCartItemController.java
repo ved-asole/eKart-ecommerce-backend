8879196ebafe142078d27e6d63d6c425a30140ce
vedasole <vedasole001@gmail.com>
2024-08-22 00:26:02 +0530

    feature: added ShoppingCartItem service implementation and controller

---
+++ src/main/java/com/vedasole/ekartecommercebackend/controller/ShoppingCartItemController.java
@@ -0,0 +1,101 @@
+package com.vedasole.ekartecommercebackend.controller;
+
+import com.vedasole.ekartecommercebackend.payload.ShoppingCartItemDto;
+import com.vedasole.ekartecommercebackend.service.serviceInterface.ShoppingCartItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Validated
+@RestController
+@RequestMapping("/api/v1/shopping-cart/{cartId}/items")
+@CrossOrigin(value = {"http://localhost:5173","https://ekart.vedasole.cloud","https://ekart-shopping.netlify.app","https://develop--ekart-shopping.netlify.app"}, allowCredentials = "true")
+@RequiredArgsConstructor
+public class ShoppingCartItemController {
+
+    private final ShoppingCartItemService shoppingCartItemService;
+
+    @PostMapping
+    public ResponseEntity<EntityModel<ShoppingCartItemDto>> createShoppingCartItem(
+            @PathVariable Long cartId,
+            @RequestBody @Valid ShoppingCartItemDto shoppingCartItemDto
+    ){
+        shoppingCartItemDto.setCartId(cartId);
+        ShoppingCartItemDto shoppingCartItem = shoppingCartItemService.createShoppingCartItem(shoppingCartItemDto);
+        EntityModel<ShoppingCartItemDto> shoppingCartItemDtoEntityModel = EntityModel.of(
+                shoppingCartItem,
+                linkTo(methodOn(ShoppingCartItemController.class).getShoppingCartItem(shoppingCartItem.getCartId(), shoppingCartItem.getCartItemId())).withSelfRel(),
+                linkTo(methodOn(ShoppingCartController.class).getShoppingCart(shoppingCartItem.getCartId())).withRel("cart")
+        );
+        return new ResponseEntity<>(shoppingCartItemDtoEntityModel, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<EntityModel<ShoppingCartItemDto>> updateShoppingCartItem(
+            @PathVariable Long cartId,
+            @RequestBody @Valid ShoppingCartItemDto shoppingCartItemDto
+    ){
+        shoppingCartItemDto.setCartId(cartId);
+        ShoppingCartItemDto shoppingCartItem = shoppingCartItemService.updateShoppingCartItem(shoppingCartItemDto);
+        EntityModel<ShoppingCartItemDto> shoppingCartItemDtoEntityModel = EntityModel.of(
+                shoppingCartItem,
+                linkTo(methodOn(ShoppingCartItemController.class).getShoppingCartItem(shoppingCartItem.getCartId(), shoppingCartItem.getCartItemId())).withSelfRel(),
+                linkTo(methodOn(ShoppingCartController.class).getShoppingCart(shoppingCartItem.getCartId())).withRel("cart")
+        );
+        return new ResponseEntity<>(shoppingCartItemDtoEntityModel, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{cartItemId}")
+    public ResponseEntity<Void> deleteShoppingCartItem(
+            @PathVariable Long cartId,
+            @PathVariable Long cartItemId
+    ) {
+        shoppingCartItemService.deleteShoppingCartItem(cartItemId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAllShoppingCartItems(
+            @PathVariable Long cartId
+    ) {
+        shoppingCartItemService.deleteAllShoppingCartItems(cartId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/{cartItemId}")
+    public ResponseEntity<EntityModel<ShoppingCartItemDto>> getShoppingCartItem(
+            @PathVariable long cartId,
+            @PathVariable long cartItemId
+    ){
+        ShoppingCartItemDto item = shoppingCartItemService.getShoppingCartItem(cartItemId);
+        EntityModel<ShoppingCartItemDto> shoppingCartItemDtoEntityModel = EntityModel.of(
+                item,
+                linkTo(methodOn(ShoppingCartItemController.class).getShoppingCartItem(item.getCartId(), item.getCartItemId())).withSelfRel(),
+                linkTo(methodOn(ShoppingCartController.class).getShoppingCart(item.getCartId())).withRel("cart")
+        );
+        return ResponseEntity.ok(shoppingCartItemDtoEntityModel);
+    }
+
+    @GetMapping
+    public ResponseEntity<CollectionModel<ShoppingCartItemDto>> getAllShoppingCartItems(
+            @PathVariable Long cartId
+    ){
+        List<ShoppingCartItemDto> allShoppingCartItems = shoppingCartItemService.getAllShoppingCartItems(cartId);
+        CollectionModel<ShoppingCartItemDto> shoppingCartItemDtoCollectionModel = CollectionModel.of(
+                allShoppingCartItems,
+                linkTo(methodOn(ShoppingCartItemController.class).getAllShoppingCartItems(cartId)).withSelfRel()
+        );
+        return ResponseEntity.ok(shoppingCartItemDtoCollectionModel);
+    }
+}
---
+++ src/main/java/com/vedasole/ekartecommercebackend/service/serviceImpl/ShoppingCartItemServiceImpl.java
@@ -0,0 +1,271 @@
+package com.vedasole.ekartecommercebackend.service.serviceImpl;
+
+import com.vedasole.ekartecommercebackend.entity.Product;
+import com.vedasole.ekartecommercebackend.entity.ShoppingCart;
+import com.vedasole.ekartecommercebackend.entity.ShoppingCartItem;
+import com.vedasole.ekartecommercebackend.exception.ResourceNotFoundException;
+import com.vedasole.ekartecommercebackend.payload.ProductDto;
+import com.vedasole.ekartecommercebackend.payload.ShoppingCartItemDto;
+import com.vedasole.ekartecommercebackend.repository.ShoppingCartItemRepo;
+import com.vedasole.ekartecommercebackend.repository.ShoppingCartRepo;
+import com.vedasole.ekartecommercebackend.service.serviceInterface.ProductService;
+import com.vedasole.ekartecommercebackend.service.serviceInterface.ShoppingCartItemService;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.vedasole.ekartecommercebackend.utility.AppConstant.RELATIONS.*;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class ShoppingCartItemServiceImpl implements ShoppingCartItemService {
+
+    private final ShoppingCartItemRepo shoppingCartItemRepo;
+    private final ProductService productService;
+    private final ShoppingCartRepo shoppingCartRepo;
+    private final Logger log = LoggerFactory.getLogger(ShoppingCartItemServiceImpl.class);
+
+    
+    /**
+     * Creates a new shopping cart item and associates it with the specified shopping cart.
+     *
+     * @param shoppingCartItemDto The shopping cart item data transfer object (DTO) containing the necessary information.
+     * @return The DTO of the newly created shopping cart item.
+     * @throws ResourceNotFoundException If the specified shopping cart does not exist.
+     */
+    @Override
+    @CacheEvict(value = "shoppingCartItems", key = "#shoppingCartItemDto.cartId")
+    public ShoppingCartItemDto createShoppingCartItem(ShoppingCartItemDto shoppingCartItemDto) {
+        log.debug("Creating shopping cart item: {}", shoppingCartItemDto);
+        ShoppingCartItem savedCartItem = shoppingCartItemRepo.save(convertToShoppingCartItem(shoppingCartItemDto));
+        savedCartItem.getShoppingCart().getShoppingCartItems().add(savedCartItem);
+        savedCartItem.getShoppingCart().calculateTotalAndDiscount();
+        log.debug("Shopping cart item created successfully: {}", savedCartItem);
+        ShoppingCart savedShoppingCart = shoppingCartRepo.save(savedCartItem.getShoppingCart());
+        log.debug("Shopping cart updated successfully: {}", savedShoppingCart);
+        savedCartItem.setShoppingCart(savedShoppingCart);
+        log.debug("Shopping cart item updated successfully: {}", savedCartItem);
+        return convertToShoppingCartItemDto(savedCartItem);
+    }
+
+    /**
+     * Creates a list of new shopping cart items and associates them with the specified shopping cart.
+     *
+     * @param shoppingCartItemDtos The list of shopping cart item data transfer objects (DTOs) containing the necessary information.
+     * @return The list of DTOs of the newly created shopping cart items.
+     */
+    @Override
+    @CacheEvict(value = "shoppingCartItems", key = "#shoppingCartItemDtos.get(0).cartId")
+    public List<ShoppingCartItemDto> createShoppingCartWithAllItems(List<ShoppingCartItemDto> shoppingCartItemDtos) {
+        List<ShoppingCartItem> savedShoppingCartItems = shoppingCartItemRepo.saveAll(
+                shoppingCartItemDtos.stream()
+                        .map(this::shoppingCartItemDtoToEntity)
+                        .toList()
+        );
+        savedShoppingCartItems.get(0).getShoppingCart().calculateTotalAndDiscount();
+        ShoppingCart savedShoppingCart = shoppingCartRepo.save(savedShoppingCartItems.get(0).getShoppingCart());
+        return savedShoppingCartItems.stream()
+                .map(shoppingCartItem -> {
+                    shoppingCartItem.setShoppingCart(savedShoppingCart);
+                    return shoppingCartItemToDto(shoppingCartItem);
+                })
+                .toList();
+    }
+
+    /**
+     * Updates the specified shopping cart item.
+     *
+     * @param shoppingCartItemDto The shopping cart item data transfer object (DTO) containing the necessary information.
+     * @return The DTO of the updated shopping cart item.
+     * @throws ResourceNotFoundException If the specified shopping cart item does not exist.
+     */
+    @Override
+    @CacheEvict(value = "shoppingCartItem", key = "#shoppingCartItemDto.cartItemId")
+    public ShoppingCartItemDto updateShoppingCartItem(ShoppingCartItemDto shoppingCartItemDto) {
+        ShoppingCart shoppingCart = shoppingCartRepo.findById(shoppingCartItemDto.getCartId())
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        SHOPPING_CART.getValue(),
+                        "cartId",
+                        shoppingCartItemDto.getCartId()
+                ));
+        return shoppingCartItemRepo.findById(shoppingCartItemDto.getCartItemId())
+                .map(shoppingCartItem -> {
+                    ProductDto product = productService.getProductById(shoppingCartItemDto.getProduct().getProductId());
+                    shoppingCartItem.setProduct(productService.productDtoToEntity(product));
+                    shoppingCartItem.setShoppingCart(shoppingCart);
+                    shoppingCartItem.setQuantity(shoppingCartItemDto.getQuantity());
+                    ShoppingCartItem savedShoppingCartItem = shoppingCartItemRepo.save(shoppingCartItem);
+                    shoppingCart.getShoppingCartItems().stream()
+                                    .filter(item -> item.getCartItemId() == savedShoppingCartItem.getCartItemId())
+                                    .findFirst()
+                                            .ifPresentOrElse(
+                                                    item -> {
+                                                        item.setCartItemId(savedShoppingCartItem.getCartItemId());
+                                                        item.setProduct(savedShoppingCartItem.getProduct());
+                                                        item.setQuantity(savedShoppingCartItem.getQuantity());
+                                                        item.setCreatedAt(savedShoppingCartItem.getCreatedAt());
+                                                        item.setUpdatedAt(savedShoppingCartItem.getUpdatedAt());
+                                                        item.setShoppingCart(shoppingCart);
+                                                    },
+                                                    () -> {
+                                                        throw new ResourceNotFoundException(
+                                                                SHOPPING_CART_ITEM.getValue(),
+                                                                "cartItemId",
+                                                                savedShoppingCartItem.getCartItemId()
+                                                        );
+                                                    }
+                                            );
+                    shoppingCart.calculateTotalAndDiscount();
+                    ShoppingCart savedShoppingCart = shoppingCartRepo.save(shoppingCart);
+                    savedShoppingCartItem.setShoppingCart(savedShoppingCart);
+                    return shoppingCartItemToDto(savedShoppingCartItem);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        SHOPPING_CART_ITEM.getValue(),
+                        "cartItemId",
+                        shoppingCartItemDto.getCartItemId()
+                ));
+    }
+
+    /**
+     * Deletes the specified shopping cart item.
+     *
+     * @param cartItemId The ID of the shopping cart item to delete.
+     * @throws ResourceNotFoundException If the specified shopping cart item does not exist.
+     */
+    @Override
+    @CacheEvict(value = "shoppingCartItem", key = "#cartItemId")
+    public void deleteShoppingCartItem(long cartItemId) {
+        this.shoppingCartItemRepo.findById(cartItemId)
+                .ifPresentOrElse(orderItem -> {
+                    orderItem.getShoppingCart().getShoppingCartItems().remove(orderItem);
+                    this.shoppingCartItemRepo.deleteById(cartItemId);
+                    orderItem.getShoppingCart().calculateTotalAndDiscount();
+                    this.shoppingCartRepo.save(orderItem.getShoppingCart());
+                    log.info("Shopping Cart Item {} deleted successfully", cartItemId);
+                }, () -> {
+                    throw new ResourceNotFoundException(SHOPPING_CART_ITEM.getValue(), "cartItemId", cartItemId);
+                });
+    }
+
+    /**
+     * Deletes all shopping cart items for a specific shopping cart.
+     *
+     * @param cartId The ID of the shopping cart for which to delete all shopping cart items.
+     */
+    @Override
+    @CacheEvict(value = "shoppingCartItems", key = "#cartId")
+    public void deleteAllShoppingCartItems(long cartId) {
+        shoppingCartItemRepo.deleteAllByShoppingCartCartId(cartId);
+        shoppingCartRepo.findById(cartId)
+                .ifPresentOrElse(
+                        shoppingCart -> {
+                            shoppingCart.setShoppingCartItems(null);
+                            shoppingCart.calculateTotalAndDiscount();
+                            shoppingCartRepo.save(shoppingCart);
+                        },
+                        () -> {
+                            throw new ResourceNotFoundException(
+                                    SHOPPING_CART.getValue(),
+                                    "cartId",
+                                    cartId
+                            );
+                        }
+                );
+        log.info("All shopping cart items for cart {} deleted successfully", cartId);
+    }
+
+    /**
+     * Retrieves a shopping cart item from the database.
+     *
+     * @param cartItemId The ID of the shopping cart item to retrieve.
+     * @return The DTO of the retrieved shopping cart item.
+     * @throws ResourceNotFoundException If the specified shopping cart item does not exist.
+     */
+    @Override
+    @Cacheable(value = "shoppingCartItem", key = "#cartItemId")
+    @Transactional(readOnly = true)
+    public ShoppingCartItemDto getShoppingCartItem(long cartItemId) {
+        return shoppingCartItemRepo.findById(cartItemId)
+                .map(this::shoppingCartItemToDto)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        SHOPPING_CART_ITEM.getValue(),
+                        "cartItemId",
+                        cartItemId
+                ));
+    }
+
+    /**
+     * Retrieves all shopping cart items for a specific shopping cart from the database.
+     *
+     * @param cartId The ID of the shopping cart for which to retrieve all shopping cart items.
+     * @return The list of DTOs of the retrieved shopping cart items.
+     */
+    @Override
+    @Cacheable(value = "shoppingCartItems", key = "#cartId")
+    @Transactional(readOnly = true)
+    public List<ShoppingCartItemDto> getAllShoppingCartItems(long cartId) {
+        return shoppingCartItemRepo.findAllByShoppingCartCartId(cartId)
+                .stream()
+                .map(this::shoppingCartItemToDto)
+                .toList();
+    }
+
+    /**
+     * Converts a ShoppingCartItemDto object to a ShoppingCartItem entity.
+     *
+     * @param shoppingCartItemDto The DTO object to be converted.
+     * @return The converted ShoppingCartItem entity.
+     */
+    @Override
+    public ShoppingCartItem convertToShoppingCartItem(ShoppingCartItemDto shoppingCartItemDto) {
+        return shoppingCartItemDtoToEntity(shoppingCartItemDto);
+    }
+
+    /**
+     * Converts a ShoppingCartItem entity to a ShoppingCartItemDto object.
+     *
+     * @param shoppingCartItem The entity object to be converted.
+     * @return The converted ShoppingCartItemDto object.
+     */
+    @Override
+    public ShoppingCartItemDto convertToShoppingCartItemDto(ShoppingCartItem shoppingCartItem) {
+        return shoppingCartItemToDto(shoppingCartItem);
+    }
+
+    private ShoppingCartItemDto shoppingCartItemToDto(ShoppingCartItem shoppingCartItem) {
+        ShoppingCartItemDto shoppingCartItemDto = new ShoppingCartItemDto();
+        shoppingCartItemDto.setCartItemId(shoppingCartItem.getCartItemId());
+        shoppingCartItemDto.setProduct(productService.productEntityToDto(shoppingCartItem.getProduct()));
+        shoppingCartItemDto.setCartId(shoppingCartItem.getShoppingCart().getCartId());
+        shoppingCartItemDto.setQuantity(shoppingCartItem.getQuantity());
+        return shoppingCartItemDto;
+    }
+
+    private ShoppingCartItem shoppingCartItemDtoToEntity(ShoppingCartItemDto shoppingCartItemDto) {
+        ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+        shoppingCartItem.setCartItemId(shoppingCartItemDto.getCartItemId());
+        shoppingCartRepo.findById(shoppingCartItemDto.getCartId())
+                        .ifPresentOrElse(
+                                shoppingCartItem::setShoppingCart,
+                                () -> {
+                                    throw new ResourceNotFoundException(
+                                            SHOPPING_CART.getValue(),
+                                            "cartId",
+                                            shoppingCartItemDto.getCartId()
+                                    );
+                                }
+                        );
+        shoppingCartItem.setQuantity(shoppingCartItemDto.getQuantity());
+        Product product = productService.productDtoToEntity(productService.getProductById(shoppingCartItemDto.getProduct().getProductId()));
+        shoppingCartItem.setProduct(product);
+        return shoppingCartItem;
+    }
+}
