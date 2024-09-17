6235f6af7e79581d1ebe7a59fe42fe7038601f57
vedasole <vedasole001@gmail.com>
2024-09-17 08:09:49 +0530

    feat(products): Add new methods to fetch all products per category by pages and use cache in ProductService
    - Add getAllProductsByPage and getAllProductsByCategory method to get products using pagination
    - update delete flow in products to catch exceptions properly
    - Add caching to the ProductService methods

--- src/main/java/com/vedasole/ekartecommercebackend/controller/ProductController.java
+++ src/main/java/com/vedasole/ekartecommercebackend/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.vedasole.ekartecommercebackend.payload.ApiResponse;
 import com.vedasole.ekartecommercebackend.payload.ProductDto;
 import com.vedasole.ekartecommercebackend.service.serviceInterface.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
+
 import static com.vedasole.ekartecommercebackend.utility.AppConstant.RELATIONS.CATEGORY;
 import static com.vedasole.ekartecommercebackend.utility.AppConstant.RELATIONS.PRODUCTS;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -20,13 +24,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @Validated
 @RestController
 @RequestMapping("/api/v1/products")
-@CrossOrigin(value = {"http://localhost:5173","https://ekart.vedasole.cloud","https://develop--ekart-shopping.netlify.app"}, allowCredentials = "true")
+@CrossOrigin(value = {"http://localhost:5173","https://ekart.vedasole.cloud","https://ekart-shopping.netlify.app","https://develop--ekart-shopping.netlify.app"}, allowCredentials = "true")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
-
-
 
     /**
      * Creates a new product.
@@ -36,7 +38,8 @@ public class ProductController {
      */
     @PostMapping
     public ResponseEntity<EntityModel<ProductDto>> createProduct(
-        @Valid @RequestBody ProductDto productDto) {
+        @Valid @RequestBody ProductDto productDto
+    ) {
         ProductDto createdProduct = this.productService.createProduct(productDto);
         EntityModel<ProductDto> productDtoEntityModel = EntityModel.of(
             createdProduct,
@@ -57,7 +60,8 @@ public class ProductController {
     @PutMapping("/{productId}")
     public ResponseEntity<EntityModel<ProductDto>> updateProduct(
         @Valid @RequestBody ProductDto productDto,
-        @PathVariable Long productId) {
+        @PathVariable Long productId
+    ) {
         ProductDto updatedProduct = this.productService.updateProduct(productDto, productId);
         EntityModel<ProductDto> productDtoEntityModel = EntityModel.of(
                 updatedProduct,
@@ -76,22 +80,19 @@ public class ProductController {
      */
     @DeleteMapping("/{productId}")
     public ResponseEntity<ApiResponse> deleteProduct(
-            @PathVariable Long productId) {
-        boolean isDeletedSuccessfully = this.productService.deleteProduct(productId);
-        return isDeletedSuccessfully
-                ? ResponseEntity.ok(
-                        new ApiResponse(
-                                "Product deleted successfully",
-                                true
-                        )
-                )
-                : new ResponseEntity<>(
-                        new ApiResponse(
-                                "Unable to delete the product",
-                                false
-                        ),
-                        HttpStatus.INTERNAL_SERVER_ERROR
-                );
+            @PathVariable Long productId
+    ) {
+        try {
+            productService.deleteProduct(productId);
+            return ResponseEntity.ok(new ApiResponse(
+                            "Product deleted successfully",
+                            true
+                    ));
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -102,7 +103,7 @@ public class ProductController {
      */
     @GetMapping("/{productId}")
     public ResponseEntity<EntityModel<ProductDto>> getProduct(
-        @PathVariable Long productId
+        @NotNull @Min(value = 0L, message = "Product id cannot be negative") @PathVariable Long productId
     ) {
         ProductDto productDto = this.productService.getProductById(productId);
         return ResponseEntity.ok(
@@ -115,16 +116,18 @@ public class ProductController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<CollectionModel<ProductDto>> getProductsByName(
-            @RequestParam String searchString
+    public ResponseEntity<CollectionModel<ProductDto>> getProductsByNameOrDesc(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "7") int size,
+            @RequestParam String searchKey
     ) {
-        List<ProductDto> productsByName = this.productService.getProductsByName(searchString);
+        List<ProductDto> productsByName = this.productService.getProductsByNameOrDesc(page, size, searchKey);
         return productsByName == null || productsByName.isEmpty() ?
-                new ResponseEntity<>(HttpStatus.NOT_FOUND) :
+                new ResponseEntity<>(HttpStatus.NO_CONTENT) :
                 new ResponseEntity<>(
                         CollectionModel.of(
                                 productsByName,
-                                linkTo(methodOn(ProductController.class).getProductsByName(searchString)).withSelfRel()
+                                linkTo(methodOn(ProductController.class).getProductsByNameOrDesc(page, size, searchKey)).withSelfRel()
                         ),
                         HttpStatus.OK
                 );
@@ -137,12 +140,43 @@ public class ProductController {
      */
     @GetMapping
     public ResponseEntity<CollectionModel<ProductDto>> getAllProducts(){
-        List<ProductDto> allProducts = this.productService.
-                getAllProducts();
+        List<ProductDto> allProducts = this.productService.getAllProducts();
         return new ResponseEntity<>(
                 CollectionModel.of(
                         allProducts,
                         linkTo(methodOn(ProductController.class).getAllProducts()).withSelfRel()
+                ),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<ProductDto>> getAllProductsByPage(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "12") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortOrder
+    ){
+        Page<ProductDto> allProducts = this.productService.getAllProductsPerPage(page, size, sortBy, sortOrder);
+//        allProducts.getContent().forEach(productDto -> productDto.setQtyInStock(0));
+        return new ResponseEntity<>(
+                allProducts,
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<CollectionModel<ProductDto>> getAllProductsByCategory(
+            @PathVariable long categoryId
+    ){
+        List<ProductDto> allProducts = this.productService.getAllProductsByCategory(categoryId);
+        allProducts.forEach(productDto -> productDto.setQtyInStock(0));
+        return new ResponseEntity<>(
+                CollectionModel.of(
+                        allProducts,
+                        linkTo(methodOn(ProductController.class).getAllProductsByCategory(categoryId)).withSelfRel(),
+                        linkTo(methodOn(CategoryController.class).getCategory(categoryId)).withRel(CATEGORY.getValue()),
+                        linkTo(methodOn(ProductController.class).getAllProducts()).withRel(PRODUCTS.getValue())
                 ),
                 HttpStatus.OK
         );
--- src/main/java/com/vedasole/ekartecommercebackend/service/serviceImpl/ProductServiceImpl.java
+++ src/main/java/com/vedasole/ekartecommercebackend/service/serviceImpl/ProductServiceImpl.java
@@ -1,33 +1,40 @@
 package com.vedasole.ekartecommercebackend.service.serviceImpl;
 
+import com.vedasole.ekartecommercebackend.entity.Category;
 import com.vedasole.ekartecommercebackend.entity.Product;
 import com.vedasole.ekartecommercebackend.exception.ResourceNotFoundException;
 import com.vedasole.ekartecommercebackend.payload.ProductDto;
 import com.vedasole.ekartecommercebackend.repository.CategoryRepo;
 import com.vedasole.ekartecommercebackend.repository.ProductRepo;
 import com.vedasole.ekartecommercebackend.service.serviceInterface.ProductService;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
+import java.util.Optional;
+
+import static com.vedasole.ekartecommercebackend.utility.AppConstant.RELATIONS.CATEGORY;
+import static com.vedasole.ekartecommercebackend.utility.AppConstant.RELATIONS.PRODUCT;
 
 
 /**
- * This class implements the ProductService interface. It uses the ProductRepo and ModelMapper objects to perform operations on the database.
+ * This class implements the ProductService interface. It uses the ProductRepo and MapperConfig objects to perform operations on the database.
  */
 @Service
+@AllArgsConstructor
+@Transactional
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepo productRepo;
     private final ModelMapper modelMapper;
     private final CategoryRepo categoryRepo;
-    private static final String CATEGORY = "Category";
-    private static final String PRODUCT = "Product";
-
-    public ProductServiceImpl(ProductRepo productRepo, ModelMapper modelMapper, CategoryRepo categoryRepo) {
-        this.productRepo = productRepo;
-        this.modelMapper = modelMapper;
-        this.categoryRepo = categoryRepo;
-    }
 
     /**
      * This method creates a new Product entity in the database and returns a ProductDto with the new entity's information.
@@ -41,7 +48,10 @@ public class ProductServiceImpl implements ProductService {
 
         product.setCategory(this.categoryRepo.findById(productDto.getCategoryId())
                 .orElseThrow(() -> new ResourceNotFoundException(
-                CATEGORY, "id" , productDto.getCategoryId())));
+                        CATEGORY.getValue(), "id" , productDto.getCategoryId()
+                        )
+                )
+        );
         // Added temp sku due to not null and not blank annotations for sku in Product
         product.setSku("temp-sku");
 
@@ -61,22 +71,41 @@ public class ProductServiceImpl implements ProductService {
      * @return a ProductDto with the updated information for the Product
      */
     @Override
+    @CacheEvict(value = "product", key = "#productId")
     public ProductDto updateProduct(ProductDto productDto, Long productId) {
         Product product = dtoToProduct(productDto);
         Product productInDB = this.productRepo.findById(productId).
                 orElseThrow(() -> new ResourceNotFoundException(
-                        PRODUCT, "id" , productId));
-        productInDB.setProductId(product.getProductId());
+                        PRODUCT.getValue(), "id" , productId));
         productInDB.setName(product.getName());
         productInDB.setImage(product.getImage());
         productInDB.setDesc(product.getDesc());
         productInDB.setPrice(product.getPrice());
         productInDB.setQtyInStock(product.getQtyInStock());
-        productInDB.setCategory(product.getCategory());
+        Optional<Category> categoryOptional = categoryRepo.findById(product.getCategory().getCategoryId());
+        categoryOptional.ifPresentOrElse(
+                productInDB::setCategory,
+                () -> {
+                    throw new ResourceNotFoundException(
+                            CATEGORY.getValue(), "id", product.getCategory().getCategoryId()
+                    );
+                }
+        );
 
         this.productRepo.save(productInDB);
 
         return productToDto(productInDB);
+    }
+
+    /**
+     * This method deletes an existing Product from the database.
+     *
+     * @param productId the ID of the Product to delete
+     */
+    @Override
+    @CacheEvict(value = "product", key = "#productId")
+    public void deleteProduct(Long productId) throws IllegalArgumentException {
+        this.productRepo.deleteById(productId);
     }
 
     /**
@@ -85,9 +114,25 @@ public class ProductServiceImpl implements ProductService {
      */
     @Override
     public List<ProductDto> getAllProducts() {
-        return this.productRepo.findAll().stream()
+        return this.productRepo.findAll(Sort.by("productId")).stream()
                 .map(this::productToDto)
                 .toList();
+    }
+
+    /**
+     * This method returns a list of ProductDtos for all Products in the database, based on the specified page, size, sort order, and sort by parameters.
+     *
+     * @param page      the page number to retrieve
+     * @param size      the number of Products to retrieve
+     * @param sortBy    the field to sort by
+     * @param sortOrder the order to sort by
+     * @return a list of ProductDtos for the specified Products
+     */
+    @Override
+    public Page<ProductDto> getAllProductsPerPage(int page, int size, String sortBy, String sortOrder) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(sortOrder), sortBy));
+        Page<Product> productsPage = productRepo.findAll(pageRequest);
+        return productsPage.map(this::productToDto);
     }
 
     /**
@@ -96,36 +141,51 @@ public class ProductServiceImpl implements ProductService {
      * @return a ProductDto for the specified Product
      */
     @Override
+    @Cacheable(value = "product", key = "#productId")
     public ProductDto getProductById(Long productId) {
         return productToDto(
                 this.productRepo.findById(productId).
                 orElseThrow(() -> new ResourceNotFoundException(
-                        PRODUCT, "id", productId)
+                        PRODUCT.getValue(), "id", productId)
                 )
         );
     }
 
+    @Override
+    public List<ProductDto> getProductsByNameOrDesc(int page, int size, String searchKey) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return this.productRepo.findByNameIsContainingIgnoreCaseOrDescContainingIgnoreCase(searchKey, searchKey, pageRequest)
+                .stream()
+                .map(this::productToDto)
+                .toList();
+    }
+
     /**
-     * This method deletes an existing Product from the database.
-     * @param productId the ID of the Product to delete
-     * @return true if the Product was deleted, false if there was an error
+     *
+     * @param categoryId
+     * @return
      */
     @Override
-    public boolean deleteProduct(Long productId) {
-        try{
-            this.productRepo.deleteById(productId);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+    public List<ProductDto> getAllProductsByCategory(long categoryId) {
+        return this.productRepo.findByCategoryCategoryId(categoryId)
+                .stream()
+                .map(this::productToDto)
+                .toList();
+    }
+
+    /**
+     * 
+     * @param productDto
+     * @return
+     */
+    @Override
+    public Product productDtoToEntity(ProductDto productDto) {
+        return dtoToProduct(productDto);
     }
 
     @Override
-    public List<ProductDto> getProductsByName(String searchString) {
-        return this.productRepo.findByNameIsContainingIgnoreCase(searchString).stream()
-                .map(this::productToDto)
-                .limit(5)
-                .toList();
+    public ProductDto productEntityToDto(Product product) {
+        return productToDto(product);
     }
 
     /**
