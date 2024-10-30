7834d5ca1df4da7d028c642d4698ed91d86aa884
vedasole <vedasole001@gmail.com>
2024-10-30 20:39:58 +0530

    refactor: add EventListener to show message once the app is up along with app name and port

--- src/main/java/com/vedasole/ekartecommercebackend/EkartEcommerceBackendApplication.java
+++ src/main/java/com/vedasole/ekartecommercebackend/EkartEcommerceBackendApplication.java
@@ -1,10 +1,16 @@
 package com.vedasole.ekartecommercebackend;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 
 /**
  * Main class of the E-Kart E-Commerce Backend application.
@@ -12,7 +18,15 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 @EnableCaching
+@Slf4j
+@RequiredArgsConstructor
 public class EkartEcommerceBackendApplication {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+
+    private final Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(EkartEcommerceBackendApplication.class, args);
@@ -29,4 +43,11 @@ public class EkartEcommerceBackendApplication {
         modelMapper.getConfiguration().setAmbiguityIgnored(true);
         return modelMapper;
     }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        log.info("EkartEcommerceBackendApplication started \uD83D\uDE80");
+        log.info("Application name: {}, Port:{}", appName, environment.getProperty("local.server.port"));
+    }
+
 }
