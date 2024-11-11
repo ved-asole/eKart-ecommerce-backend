eb5b9649985df1604f7d2dd3c2d29fd9d073700a
vedasole <vedasole001@gmail.com>
2024-11-12 01:10:25 +0530

    refactor(test/category): update expected category to have static date of 2022-01-01 01:00

--- src/test/java/com/vedasole/ekartecommercebackend/controller/CategoryControllerTest.java
+++ src/test/java/com/vedasole/ekartecommercebackend/controller/CategoryControllerTest.java
@@ -55,8 +55,8 @@ class CategoryControllerTest {
                 "Mobile phones are no more merely a part of our lives. Whether it's to stay connected with friends and family or to keep abreast of important developments around the world, mobiles are no longer for sending a text or making a call. From budget to state-of-the-art smartphones; indigenous names to global big-wigs - a whole universe of mobiles await you on Flipkart. Whether you’re looking for waterdrop notch screens, a high screen to body ratio, AI-powered sensational cameras, high storage capacity, blazing quick processing engines or reflective glass designs, rest assured you won’t have to venture anywhere else for your smartphone needs.",
                 null,
                 true,
-                LocalDateTime.now(),
-                LocalDateTime.now()
+                LocalDateTime.of(2022, 1, 1, 1, 0),
+                LocalDateTime.of(2022, 1, 1, 1, 0)
         );
     }
 
