5f83993d922fc741b8dc17aef64b5179e12affae
vedasole <vedasole001@gmail.com>
2024-11-23 07:29:03 +0530

    feat(reset-password): add PasswordResetToken entity with repository and PasswordResetRequestDto for capturing and sending token for password reset

---
+++ src/main/java/com/vedasole/ekartecommercebackend/entity/PasswordResetToken.java
@@ -0,0 +1,40 @@
+package com.vedasole.ekartecommercebackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reset_token")
+public class PasswordResetToken {
+
+    @Id
+    @Column(updatable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reset_token_seq")
+    @SequenceGenerator(name = "reset_token_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    public PasswordResetToken(String token, String email, LocalDateTime expiryDate) {
+        this.token = token;
+        this.email = email;
+        this.expiryDate = expiryDate;
+    }
+
+}
---
+++ src/main/java/com/vedasole/ekartecommercebackend/repository/PasswordResetTokenRepo.java
@@ -0,0 +1,14 @@
+package com.vedasole.ekartecommercebackend.repository;
+
+import com.vedasole.ekartecommercebackend.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepo extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+
+}
