0bc3919d92d6226e49bdbaee81a234c14bbe0397
Ved Asole <vedasole001@gmail.com>
2026-05-23 02:01:40 +0530

    feat(order): update getTotalIncomeByMonth query to return MonthlyIncomeDto with formatted date

--- src/main/java/com/vedasole/ekartecommercebackend/repository/OrderRepo.java
+++ src/main/java/com/vedasole/ekartecommercebackend/repository/OrderRepo.java
@@ -17,11 +17,14 @@ public interface OrderRepo extends JpaRepository<Order, Long> {
     @Query(value = "SELECT COALESCE(SUM(o.\"total\"), 0) FROM \"order\" o", nativeQuery = true)
     Double getTotalIncome();
 
-    @Query(
-            value = "SELECT TO_CHAR(o.\"create_dt\", 'YYYY-MM') AS date, SUM(o.\"total\") AS income " +
-                    "FROM \"order\" o GROUP BY TO_CHAR(o.\"create_dt\", 'YYYY-MM') " +
-                    "ORDER BY date"
-            , nativeQuery = true
+    @Query(value = """
+    SELECT new com.vedasole.ekartecommercebackend.payload.MonthlyIncomeDto(
+        CONCAT(CAST(YEAR(o.createdAt) AS string), '-', LPAD(CAST(MONTH(o.createdAt) AS string), 2, '0')),
+        SUM(o.total)
     )
+    FROM Order o
+    GROUP BY CONCAT(CAST(YEAR(o.createdAt) AS string), '-', LPAD(CAST(MONTH(o.createdAt) AS string), 2, '0'))
+    ORDER BY CONCAT(CAST(YEAR(o.createdAt) AS string), '-', LPAD(CAST(MONTH(o.createdAt) AS string), 2, '0')) ASC
+    """)
     List<MonthlyIncomeDto> getTotalIncomeByMonth();
 }
